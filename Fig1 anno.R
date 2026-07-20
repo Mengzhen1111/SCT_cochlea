0a0cdbec39b01c41e87e607dbe13ae56299ace1f
Mengzhen1111 <2017810528@qq.com>
2026-07-20 22:50:45 +0800

    Update Fig1 anno.R

--- Fig1 anno.R
+++ Fig1 anno.R
@@ -149,6 +149,155 @@ object6 <- FindClusters(object=object6, verbose = F,resolution = 1)
 object6 <- RunUMAP(object=object6, reduction = "pca",dims = 1:14)
 object6$sample <- "Adult_right"
 saveRDS(object6,"Adult_SCT_right.Rds")
+###############################QC#########################################
+# ===================================  E17.5========================
+object1 <- readRDS("E17_SCT_R.Rds")
+object1 <- PercentageFeatureSet(object1, "^mt-", col.name = "percent_mito")
+saveRDS(object1,"E17_SCT_R.Rds")
+semla.data1 <- UpdateSeuratForSemla(object1)
+semla.data1 <- LoadImages(semla.data1, verbose = FALSE)
+MapFeaturesSummary(semla.data1, features = "percent_mito", ncol = 1,
+                   subplot_type = "violin",
+                   pt_alpha = 5,
+                   pt_size = 1,
+                   pt_stroke = 0.001,
+                   crop_area = c(0.55, 0.55, 0.75, 0.71)
+)&theme(legend.position = "right") 
+
+genes_to_plot <- c("nCount_Spatial", "nFeature_Spatial", "percent_mito")
+
+plot_list <- lapply(genes_to_plot, function(g){
+  MapFeaturesSummary(semla.data1, features = g, ncol = 1,
+                     subplot_type = "violin",
+                     pt_alpha = 5,
+                     pt_size = 1,
+                     pt_stroke = 0.001,
+                     crop_area = c(0.55, 0.55, 0.75, 0.71)
+  )&theme(legend.position = "right") 
+})
+pdf("SCT_E17.5_R_QC.pdf",width =18,height = 5)
+wrap_plots(plot_list, nrow = 1)
+dev.off()
+
+object1 <- readRDS("E17_SCT_L.Rds")
+object1 <- PercentageFeatureSet(object1, "^mt-", col.name = "percent_mito")
+saveRDS(object1,"E17_SCT_L.Rds")
+
+semla.data1 <- UpdateSeuratForSemla(object1)
+semla.data1 <- LoadImages(semla.data1, verbose = FALSE)
+MapFeaturesSummary(semla.data1, features = "nCount_Spatial", ncol = 1,
+                   subplot_type = "violin",
+                   pt_alpha = 5,
+                   pt_size = 1,
+                   pt_stroke = 0.001,
+                   crop_area = c(0.33, 0.34, 0.49, 0.53)
+)&theme(legend.position = "right") 
+
+genes_to_plot <- c("nCount_Spatial", "nFeature_Spatial", "percent_mito")
+
+plot_list <- lapply(genes_to_plot, function(g){
+  MapFeaturesSummary(semla.data1, features = g, ncol = 1,
+                     subplot_type = "violin",
+                     pt_alpha = 5,
+                     pt_size = 1,
+                     pt_stroke = 0.001,
+                     crop_area = c(0.33, 0.34, 0.49, 0.53)
+  )&theme(legend.position = "right") 
+})
+wrap_plots(plot_list, nrow = 1)
+pdf("SCT_E17.5_L_QC.pdf",width =17,height = 5)
+wrap_plots(plot_list, nrow = 1)
+dev.off()
+
+# ===================================  P8 ========================
+object1 <- readRDS("P8_SCT_up_anno.Rds")
+object1 <- PercentageFeatureSet(object1, "^mt-", col.name = "percent_mito")
+saveRDS(object1,"P8_SCT_up_anno.Rds")
+
+semla.data1 <- UpdateSeuratForSemla(object1)
+semla.data1 <- LoadImages(semla.data1, verbose = FALSE)
+MapFeaturesSummary(semla.data1, features = "percent_mito", ncol = 1,
+                   subplot_type = "violin",
+                   pt_alpha = 5,
+                   pt_size = 1,
+                   pt_stroke = 0.001,
+                   crop_area = c(0.4, 0.18, 0.65, 0.45)
+)&theme(legend.position = "right")
+
+genes_to_plot <- c("nCount_Spatial", "nFeature_Spatial", "percent_mito")
+
+plot_list <- lapply(genes_to_plot, function(g){
+  MapFeaturesSummary(semla.data1, features = g, ncol = 1,
+                     subplot_type = "violin",
+                     pt_alpha = 5,
+                     pt_size = 0.9,
+                     pt_stroke = 0.001,
+                     crop_area = c(0.4, 0.18, 0.65, 0.45)
+  )&theme(legend.position = "right") 
+})
+pdf("SCT_P8_Up_QC.pdf",width =20,height = 5)
+wrap_plots(plot_list, nrow = 1)
+dev.off()
+
+object1 <- readRDS("P8_SCT_Down_anno.Rds")
+object1 <- PercentageFeatureSet(object1, "^mt-", col.name = "percent_mito")
+saveRDS(object1,"P8_SCT_Down_anno.Rds")
+
+semla.data1 <- UpdateSeuratForSemla(object1)
+semla.data1 <- LoadImages(semla.data1, verbose = FALSE)
+MapFeaturesSummary(semla.data1, features = "nCount_Spatial", ncol = 1,
+                   subplot_type = "violin",
+                   pt_alpha = 5,
+                   pt_size = 0.9,
+                   pt_stroke = 0.001,
+                   crop_area = c(0.45, 0.55, 0.715, 0.835)
+)&theme(legend.position = "right") 
+
+genes_to_plot <- c("nCount_Spatial", "nFeature_Spatial", "percent_mito")
+
+plot_list <- lapply(genes_to_plot, function(g){
+  MapFeaturesSummary(semla.data1, features = g, ncol = 1,
+                     subplot_type = "violin",
+                     pt_alpha = 5,
+                     pt_size = 0.9,
+                     pt_stroke = 0.001,
+                     crop_area = c(0.45, 0.55, 0.715, 0.835)
+  )&theme(legend.position = "right") 
+})
+wrap_plots(plot_list, nrow = 1)
+pdf("SCT_P8_Down_QC.pdf",width =20,height = 5)
+wrap_plots(plot_list, nrow = 1)
+dev.off()
+
+# =================================== Adult ========================
+object1 <- readRDS("Adult_SCT_right_anno.Rds")
+object1 <- PercentageFeatureSet(object1, "^mt-", col.name = "percent_mito")
+saveRDS(object1,"Adult_SCT_right_anno.Rds")
+
+semla.data1 <- UpdateSeuratForSemla(object1)
+semla.data1 <- LoadImages(semla.data1, verbose = FALSE)
+MapFeaturesSummary(semla.data1, features = "percent_mito", ncol = 1,
+                   subplot_type = "violin",
+                   pt_alpha = 5,
+                   pt_size = 0.9,
+                   pt_stroke = 0.001,
+                   crop_area = c(0.55, 0.49, 0.85, 0.685)
+)&theme(legend.position = "right")
+
+genes_to_plot <- c("nCount_Spatial", "nFeature_Spatial", "percent_mito")
+
+plot_list <- lapply(genes_to_plot, function(g){
+  MapFeaturesSummary(semla.data1, features = g, ncol = 1,
+                     subplot_type = "violin",
+                     pt_alpha = 5,
+                     pt_size = 0.8,
+                     pt_stroke = 0.001,
+                     crop_area = c(0.55, 0.49, 0.85, 0.685)
+  )&theme(legend.position = "right") 
+})
+pdf("SCT_Adult_Right_QC.pdf",width =20,height = 5)
+wrap_plots(plot_list, nrow = 1)
+dev.off()
 
 ###################################### Data annotation #################################
 
